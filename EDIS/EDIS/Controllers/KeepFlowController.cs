dfc593bc90ca221c94d41a25afff238b2e607c98
JimyHsu <airball91327@hotmail.com>
2021-01-05 16:10:48 +0800

    20210105 Update.

--- EDIS/EDIS/Controllers/KeepFlowController.cs
+++ EDIS/EDIS/Controllers/KeepFlowController.cs
@@ -468,7 +468,8 @@ namespace EDIS.Controllers
                     break;
                 case "列管財產負責人":
                     list = new List<SelectListItem>();
-                    u = _context.AppUsers.Where(ur => ur.UserName == "181151").FirstOrDefault();
+                    //u = _context.AppUsers.Where(ur => ur.UserName == "181151").FirstOrDefault();
+                    u = _context.AppUsers.Where(ur => ur.UserName == "111218").FirstOrDefault();
                     if (!string.IsNullOrEmpty(u.DptId))
                     {
                         li = new SelectListItem();
--- EDIS/EDIS/Controllers/RepairFlowController.cs
+++ EDIS/EDIS/Controllers/RepairFlowController.cs
@@ -613,7 +613,8 @@ namespace EDIS.Controllers
                 case "列管財產負責人":
                     list = new List<SelectListItem>();
                     //u = _context.AppUsers.Where(ur => ur.UserName == "181151").FirstOrDefault();
-                    u = _context.AppUsers.Where(ur => ur.UserName == "183436").FirstOrDefault();
+                    //u = _context.AppUsers.Where(ur => ur.UserName == "183436").FirstOrDefault();
+                    u = _context.AppUsers.Where(ur => ur.UserName == "111218").FirstOrDefault();
                     if (!string.IsNullOrEmpty(u.DptId))
                     {
                         li = new SelectListItem();
